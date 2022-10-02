70e704eabf4d24cba3177280ca0e4b8c79400786
Rosie Kwon <76680071+rosiekwon@users.noreply.github.com>
2022-10-02 15:07:04 -0400

    Update and rename exercise1.java to Lab 0/exercise1.java

---
+++ Lab 0/exercise1.java
@@ -0,0 +1,11 @@
+package labs.lab0;
+
+public class exercise1 {
+
+	public static void main(String[] args) {
+		System.out.println("My name is Rosie Kwon and nice to meet you!");
+		//This example prints out my name
+
+	}
+
+}
